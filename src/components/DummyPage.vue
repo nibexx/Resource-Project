a788f0c316273cf4838fa6410a901c504166ec74
akhila_l <akhila04.ptf@gmail.com>
2024-08-02 10:46:39 +0530

    aa

--- src/components/DummyPage.vue
+++ src/components/DummyPage.vue
@@ -1,6 +1,6 @@
+
 <template>
   <div id="root">
-    <!-- Header Component -->
     <header-page></header-page>
 
     <!-- Filter Dropdowns -->
@@ -12,13 +12,7 @@
         </label>
         <select id="categoryDropdown" v-model="selectedCategory">
           <option value="">Filter Category</option>
-          <option
-            class="dropdown"
-            v-for="category in categories"
-            :key="category"
-          >
-            {{ category }}
-          </option>
+          <option class="dropdown" v-for="category in categories" :key="category">{{ category }}</option>
         </select>
       </div>
 
@@ -29,19 +23,13 @@
         </label>
         <select id="districtDropdown" v-model="selectedDistrict">
           <option value="">Filter Districts</option>
-          <option
-            class="dropdown"
-            v-for="district in districts"
-            :key="district"
-          >
-            {{ district }}
-          </option>
+          <option class="dropdown" v-for="district in districts" :key="district">{{ district }}</option>
         </select>
       </div>
     </div>
 
     <!-- Card Container Component -->
-    <div class="card-container mb-3" v-motion-fade-visible v-if="allCities.length">
+    <div class="card-container mb-3" v-motion-fade-visible>
       <div
         class="card"
         v-for="(city, index) in filteredCities"
@@ -52,19 +40,12 @@
         <!-- Card Front -->
         <div class="card-front">
           <div class="card-header" @click.stop="openModal(city.image)">
-            <img
-              :src="'data:image/jpeg;base64,' + city.image"
-              :width="310.92"
-              :height="215.02"
-            />
+            <img :src="'data:image/jpeg;base64,' + city.image" :width="310.92" :height="215.02" />
           </div>
           <div class="card-body pb-0">
             <div class="d-flex justify-content-between">
               <h3>{{ city.saverName }}</h3>
-              <button
-                @click="navLocation(city.id)"
-                class="location-btn mdi mdi-map-marker-radius"
-              ></button>
+              <button @click="navLocation(city.id)" class="location-btn mdi mdi-map-marker-radius"></button>
             </div>
             <p class="text-subtitle-1">Category: {{ city.category }}</p>
             <p class="text-subtitle-2 mb-2">District: {{ city.district }}</p>
@@ -88,28 +69,23 @@
       <!-- Modal for Image -->
       <v-dialog v-model="showModal" @click="closeModal" width="100%" height="600">
         <v-btn class="close-btn" @click="closeModal" icon>
-          <v-icon>mdi-close</v-icon>
-        </v-btn>
+            <v-icon>mdi-close</v-icon>
+          </v-btn>
         <v-card>
-          <v-img
-            :src="'data:image/jpeg;base64,' + modalImage"
-            @click.stop
-            cover
-            height="100vh"
-          ></v-img>
+          <v-img :src="'data:image/jpeg;base64,' + modalImage" @click.stop cover height="100vh"></v-img>
         </v-card>
       </v-dialog>
     </div>
-
+    <br />
     <!-- Footer Component -->
     <footer-page></footer-page>
   </div>
 </template>
 
 <script>
-import axios from "axios";
-import FooterPage from "./FooterPage.vue";
-import HeaderPage from "./HeaderPage.vue";
+import axios from 'axios';
+import FooterPage from './FooterPage.vue';
+import HeaderPage from './HeaderPage.vue';
 
 export default {
   components: {
@@ -120,35 +96,25 @@ export default {
     return {
       allCities: [], // Store all cities data
       showModal: false,
-      modalImage: "",
-      selectedCategory: "",
-      selectedDistrict: "",
-      categories: [
-        "Lake",
-        "Pond",
-        "Tree",
-        "Seashore",
-        "Soil",
-        "Canal",
-        "Plantation",
-        "Garden",
-        "River",
-      ],
+      modalImage: '',
+      selectedCategory: '',
+      selectedDistrict: '',
+      categories: ['Lake', 'Pond', 'Tree', 'Seashore', 'Soil', 'Canal', 'Plantation', 'Garden','River'],
       districts: [
-        "Alappuzha",
-        "Ernakulam",
-        "Idukki",
-        "Kannur",
-        "Kasaragod",
-        "Kollam",
-        "Kottayam",
-        "Kozhikode",
-        "Malappuram",
-        "Palakkad",
-        "Pathanamthitta",
-        "Thiruvananthapuram",
-        "Thrissur",
-        "Wayanad",
+        'Alappuzha',
+        'Ernakulam',
+        'Idukki',
+        'Kannur',
+        'Kasaragod',
+        'Kollam',
+        'Kottayam',
+        'Kozhikode',
+        'Malappuram',
+        'Palakkad',
+        'Pathanamthitta',
+        'Thiruvananthapuram',
+        'Thrissur',
+        'Wayanad',
       ],
     };
   },
@@ -159,40 +125,32 @@ export default {
     filteredCities() {
       // Filter cities based on selectedCategory and selectedDistrict
       return this.allCities.filter((city) => {
-        const categoryMatch =
-          !this.selectedCategory || city.category === this.selectedCategory;
-        const districtMatch =
-          !this.selectedDistrict || city.district === this.selectedDistrict;
+        const categoryMatch = !this.selectedCategory || city.category === this.selectedCategory;
+        const districtMatch = !this.selectedDistrict || city.district === this.selectedDistrict;
         return categoryMatch && districtMatch;
       });
     },
   },
   methods: {
     navLocation(locationId) {
-      this.$router.push({ path: "/locations", query: { locationId } });
+      this.$router.push({ path: '/locations', query: { locationId } });
     },
     async fetchCities() {
       try {
-        const response = await axios.get("http://192.168.1.18:8080/GreenGuard/getAll");
-        console.log("Fetched cities:", response.data);
-        // Wrap data update in nextTick to ensure reactivity
-        this.$nextTick(() => {
-          this.allCities = response.data.map((city) => ({
-            ...city,
-            showDetails: false, // Initialize showDetails for each city
-          }));
-        });
+        const response = await axios.get('http://192.168.1.30:8080/GreenGuard/getAll');
+        console.log(response.data);
+        this.allCities = response.data;
       } catch (error) {
-        console.error("Error fetching cities:", error);
+        console.error('Error fetching cities:', error);
       }
     },
     toggleDetails(city) {
       city.showDetails = !city.showDetails;
     },
     shortenedDescription(description) {
-      const words = description.split(" ");
+      const words = description.split(' ');
       if (words.length > 16) {
-        return words.slice(0, 16).join(" ") + "...";
+        return words.slice(0, 16).join(' ') + '...';
       } else {
         return description;
       }
@@ -203,7 +161,7 @@ export default {
     },
     closeModal() {
       this.showModal = false;
-      this.modalImage = "";
+      this.modalImage = '';
     },
   },
 };
@@ -211,13 +169,15 @@ export default {
 
 <style scoped>
 #root {
-  background-color: #8bc34a;
-  background-image: url("~@/assets/b2.jpg"); /* Adjust path if necessary */
+  background-color: #8BC34A; 
+  background-image: url('~@/assets/b2.jpg'); /* Adjust path if necessary */
   background-size: cover; /* Adjust to cover the entire container */
   background-position: center; /* Center the background image */
   min-height: 100vh;
   min-height: 100vh; /* Ensure the background covers the full height */
 }
+
+
 
 /* Filters Styles */
 .filters-container {
@@ -283,80 +243,171 @@ select {
 .card {
   margin: 1em 0;
   border-radius: 1rem;
-  background-color: #fff;
-  box-shadow: 0 2px 5px rgba(0, 0, 0, 0.1);
-  overflow: hidden;
-  transition: transform 0.5s;
-  transform-style: preserve-3d;
+  background: #fff;
+  box-shadow: 0 8px 16px rgba(0, 0, 0, 0.1);
+  min-height: 487px;
+  display: grid;
+  grid-template-rows: 1fr;
   cursor: pointer;
+  transition: transform 0.8s cubic-bezier(0.165, 0.84, 0.44, 1);
+  transform-style: preserve-3d;
+  position: relative;
+  perspective: 1000px;
+  width: 325px;
 }
-.card:hover {
-  transform: scale(1.05);
+.card-front,
+.card-back {
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  backface-visibility: hidden;
+  border: 2px solid #000;
+  border-radius: 1rem;
+  background: #fff;
+}
+.card-front {
+  z-index: 2;
+  transform: rotateY(0deg);
+}
+.card-back {
+  transform: rotateY(180deg);
+  color: #030303;
+  padding: 1rem;
 }
 .card.is-flipped {
   transform: rotateY(180deg);
 }
-.card-front,
-.card-back {
-  width: 100%;
-  height: 100%;
-  backface-visibility: hidden;
+.card-header {
+  aspect-ratio: 16/12;
+  overflow: hidden;
+  border-radius: 1rem 1rem 0 0;
+  position: relative;
 }
-.card-back {
+.card-header::after {
+  content: "";
   position: absolute;
-  top: 0;
-  left: 0;
-  transform: rotateY(180deg);
+  inset: auto 0 0 auto;
+  margin-bottom: .5em;
+  font-size: 6em;
+  color: var(--accent);
+  font-weight: 800;
+  opacity: .75;
 }
-.card-header img {
-  width: 100%;
-  height: auto;
+.card img {
+  min-height: 100%;
+  max-inline-size: 100%;
   object-fit: cover;
 }
 .card-body {
-  padding: 1em;
+  padding: 1rem;
+  height: 220px;
 }
-.scrollable-description {
-  max-height: 200px;
-  overflow-y: auto;
+.card-footer {
+  display: grid;
+  align-items: center;
+}
+.card h3 {
+  margin: 0;
+  position: relative;
+  font-size: 2rem;
+  font-weight: 600;
+}
+.card-body p {
+  font-weight: 200;
+  font-size: 1rem;
+  margin: 0;
+}
+.btn {
+  font-weight: regular;
+  border-radius: .5rem;
+  padding: 14px 20px;
+  font-size: 13px;
+  outline: none;
+  max-width: 42%;
+  cursor: pointer;
+  text-align: center;
+  display: inline-block;
+  transition: all .3s;
+  background-color: transparent;
+  border: 1px solid var(--accent);
+  color: var(--accent);
 }
 .location-btn {
-  color: #3b5998;
-  border: none;
-  background: none;
-  cursor: pointer;
-}
-.location-btn:focus {
-  outline: none;
-}
-
-.btn {
-  border: none;
+  border: 1px solid #007bff;
+  color: #007bff;
   background-color: transparent;
-  color: #1976d2;
-  font-weight: bold;
-  cursor: pointer;
-  text-transform: uppercase;
+  padding: 4px 10px;
+  border-radius: 4px;
+  margin-top: 8px;
+  margin-bottom: 5px;
 }
-
-.v-dialog {
+.scrollable-description {
+  max-height: 370px;
+  overflow-y: auto;
+}
+.scrollable-description::-webkit-scrollbar {
+  width: 2px;
+}
+.scrollable-description::-webkit-scrollbar-track {
+  background: #f1f1f1;
+  border-radius: 10px;
+}
+.scrollable-description::-webkit-scrollbar-thumb {
+  background: #888;
+  border-radius: 10px;
+}
+.scrollable-description::-webkit-scrollbar-thumb:hover {
+  background: #555;
+}
+.scrollable-description {
+  scrollbar-width: thin;
+  scrollbar-color: #888 #f1f1f1;
+}
+.modal {
   display: flex;
   justify-content: center;
   align-items: center;
-  background: rgba(0, 0, 0, 0.8);
+  position: fixed;
+  z-index: 1000;
+  left: 0;
+  top: 0;
+  width: 100%;
+  height: 100%;
+  overflow: hidden; /* Ensure no scrollbars show */
+  background-color: rgba(0, 0, 0, 0.8);
 }
 
-.v-card {
-  background: transparent;
-  box-shadow: none;
+.modal-content {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: transparent;
+  border: none;
+  width: 500px; /* Responsive width */
+ /* Maximum width for larger screens */
+  height:300px; /* Responsive height */
+  /* Maximum height for larger screens */
+  transform: translateY(-800px); 
 }
 
-.v-btn.close-btn {
+.modal-content img {
+  max-width: 100%; /* Scale the image to fit the modal */
+  max-height: 100%;
+  object-fit: contain; /* Ensures the image maintains its aspect ratio */
+  border-radius: 1rem;
+}
+
+.close-btn {
   position: absolute;
   top: 10px;
   right: 10px;
-  color: #ffffff;
-  background-color: rgba(0, 0, 0, 0.6);
-  border-radius: 50%;
+  color: #070707; /* Adjust color if needed */
+  z-index: 1001; /* Ensure the button is above other elements */
 }
+
+.close-btn .v-icon {
+  font-size: 1.5rem;
+  
+}
+
 </style>
